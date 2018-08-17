51d6d1554aa804cacd71a19c16357ba3d15f9a1e
Mitch Hankins <mithan@us17mac00240.org.nasdaqomx.com>
2018-08-17 08:43:10 -0600

    Adding member superclass

---
+++ src/fitnessclubmanager/Member.java
@@ -0,0 +1,55 @@
+package fitnessclubmanager;
+
+public class Member {
+    private char memberType;
+    private int memberID;
+    private String name;
+    private double fees;
+
+    public Member(char pMemberType, int pMemberID, String pName, double pFees) {
+        memberType = pMemberType;
+        memberID = pMemberID;
+        name = pName;
+        fees = pFees;
+    }
+
+    public char getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(char pMemberType) {
+        memberType = pMemberType;
+    }
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int pMemberID) {
+        memberID = pMemberID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String pName) {
+        name = pName;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double pFees) {
+        fees = pFees;
+    }
+
+    @Override
+    public String toString() {
+        String output = String.format(
+                "Type: %c, ID: %d, Name: %s, Fees: %d", memberType, memberID, name, fees
+        );
+        return output;
+    }
+}
